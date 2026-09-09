833b61c0b61c65b7cdcf2f58c61f75b0cda1bb95
Peng <peng.it@outlook.com>
2026-09-09 14:07:21 +1000

    allow external access

--- job-scheduler/backend/flask_server.py
+++ job-scheduler/backend/flask_server.py
@@ -22,4 +22,4 @@ def run_jobs():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
